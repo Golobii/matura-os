e8da691158caffa3e1c6b9ed145684ad44bfd7ba
golobii <luka@golobii.com>
2024-03-21 22:01:58 +0100

    dodal logo

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # MATURA-OS
 
-Moja maturitetna naloga.
+![slika sistema](printscr.png)
+
+Operacijski sistem, ki ga izdelujem za mojo maturitetno nalogo.
+
+Cilji:
+- [x] bootloader
+- [x] preprosto jedro
+- [ ] rezerviranje spomina
+- [ ] uporabniški vnos
+- [ ] preprosta školjka
+- [ ] zagon izven emulatorja
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,22 @@
 #include "../drivers/display.h"
 
+void printLogo();
+
 void kernel() {
     init_tty();
-    putstr("Pozdravljeni v MaturaOS - najbolj povprecnem operacijskem sistemu");
+    printLogo();
+
+    putstr("\n\n\n$> ");
+}
+
+
+void printLogo() {
+    putstr(" __  __       _                    ___  ____\n");
+    putstr("|  \\/  | __ _| |_ _   _ _ __ __ _ / _ \\/ ___|\n");
+    putstr("| |\\/| |/ _` | __| | | | '__/ _` | | | \\___ \\\n");
+    putstr("| |  | | (_| | |_| |_| | | | (_| | |_| |___) |\n");
+    putstr("|_|  |_|\\__,_|\\__|\\__,_|_|  \\__,_|\\___/|____/\n");
+    putchar('\n');
     putstr("Pozdravljeni v MaturaOS - najbolj povprecnem operacijskem sistemu");
 }
+
